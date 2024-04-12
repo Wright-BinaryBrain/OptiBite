68fb960f1d86de712416b7bbfa517e8f1c08270c
Chirag Simkhada <chiragsimkhada@gmail.com>
2024-04-12 12:00:40 -0400

    Recommendation API Implemented for Backend

--- backend/controller/recommendationController.js
+++ backend/controller/recommendationController.js
@@ -1,35 +1,43 @@
+const axios = require("axios");
 const ErrorHandler = require("../utils/errorHandler");
 const APIFeatures = require("../utils/apiFeatures");
 
-const ProductType = require("../Model/ProductType");
 const Product = require("../Model/Product");
+const Order = require("../Model/Order");
 
-// exports.postProductType = catchAsyncErrors(async (req, res, next) => {
-//   const newProductType = req.body;
-//   const productType = await ProductType(newProductType).save();
-//   res.status(200).json({
-//     success: true,
-//     message: "Product Type added successfully!",
-//     data: productType,
-//   });
-// });
+
 
 exports.getUserOrders = async(req,res)=>{
     const user = req.user;
     if(!user){
         // get most expensive products
         const product = await Product.find().sort({Rate:-1}).limit(1);
-        console.log(product);
+        const name = product[0].Name.replace(/ /g, "%20");
+        
+        const recommendation = await axios.get(`http://localhost:8000/rec/${name}`);
+        // console.log();
+        return res.status(200).json({
+            success: true,
+            message: "User Orders fetched successfully!",
+            data: recommendation.data.items,
+        });
     }
-    // const userOrders = await Order.find({user:user._id}).populate('orderItems.product');
-    // res.status(200).json({
-    //     success: true,
-    //     message: "User Orders fetched successfully!",
-    //     data: userOrders,
-    // });
+    else{
+        // get user orders
+        const userOrders = await Order.find({user:user._id}).populate('productId');
+        //loop through products and get recommendations
+        const recs = [];
+        for(let i=0;i<userOrders.length;i++){
+            const response = await axios.get(`http://localhost:8000/rec/${userOrders[i].productId[0].Name}`)
+            for (let j = 0; j < response.data.items.length; j++) {
+                recs.push(response.data.items[j]);
+            }
+        }
+        return res.status(200).json({
+            success: true,
+            message: "User Orders fetched successfully!",
+            data: recs,
+        });
+    }
 
-    res.status(200).json({
-        success: true,
-        message: "User Orders fetched successfully!"
-    });
 }
