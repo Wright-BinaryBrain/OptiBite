76dbc4da83f5ede66596735a03b5907bdc96b63e
Chirag Simkhada <chiragsimkhada@gmail.com>
2024-04-14 12:12:42 -0400

    Scheduled Order

---
+++ backend/router/scheduleOrderRouter.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+    postScheduleOrder,
+    getAllScheduledOrders,
+    getOneScheduleOrder,
+    deleteScheduleOrder,
+    updateScheduleOrder,
+    } = require("../controller/scheduleOrderController");
+
+const { isAuthenticated, authorizeRoles } = require("../middleware/auth");
+// const scheduleOrderValidation = require("../validation/scheduleOrderValidation");
+
+router
+    .route("/scheduleOrder")
+    .post(
+        postScheduleOrder
+    );
+
+router
+    .route("/getAllScheduledOrders")
+    .get(
+        isAuthenticated, 
+        authorizeRoles("superAdmin", "admin"), 
+        getAllScheduledOrders
+    );
+
+router
+    .route("/getOneScheduleOrder/:id")
+    .get(
+        isAuthenticated, 
+        authorizeRoles("superAdmin", "admin"), 
+        getOneScheduleOrder
+    );
+
+// router
+//     .route("/updateScheduleOrder/:id")
+//     .patch(
+//         isAuthenticated, 
+//         authorizeRoles("superAdmin"), 
+//         scheduleOrderValidation, 
+//         updateScheduleOrder
+//     );
+
+router
+    .route("/deleteScheduleOrder/:id")
+    .delete(
+        isAuthenticated, 
+        authorizeRoles("superAdmin"), 
+        deleteScheduleOrder
+    );
+
+router
+    .route("/getScheduleByUser/:id")
+    .get(
+        isAuthenticated, 
+        authorizeRoles("user"), 
+        getScheduleByUser
+    );
+
+router
+    .route("/gerScheduleByStatus/:status")
+    .get(
+        isAuthenticated, 
+        authorizeRoles("superAdmin", "admin"), 
+        getScheduleByStatus
+    );
+
+router
+    .route("/getScheduleByDate/:startDate/:endDate")
+    .get(
+        isAuthenticated, 
+        authorizeRoles("superAdmin", "admin"), 
+        getScheduleByDate
+    );
+
+module.exports = router;
