027517aa25fd003af47f89eed4d90a5acbfd1423
SuhasR2801 <163041558+SuhasR2801@users.noreply.github.com>
2024-04-15 06:12:35 +0530

    Add files via upload

---
+++ backend/validation/scheduleOrderValidation.js
@@ -0,0 +1,26 @@
+const joi = require("joi");
+
+const scheduleOrderSchema = joi.object({
+    startDate: joi.date().required(true),
+    endDate: joi.date().required(true),
+    timeOfDelivery: joi.string().required(true),
+    productId: joi.array().items(joi.string()).required(true),
+    userId: joi.string().required(true),
+    quantity: joi.number().required(true),
+    rates: joi.number().required(false),
+    totalAmount: joi.number().required(true),
+    orderAddress: joi.string().required(true),
+    orderStatus: joi.string().valid("Pending", "In Process", "Being Delivered", "Completed", "Cancelled").default("Pending")
+    });
+
+    const scheduleOrderValidation = (req, res, next) => {
+        const scheduleOrderObject = req.body;
+        const { err } = scheduleOrderSchema.validate(scheduleOrderObject);
+        if (!err) {
+            next();
+        } else {
+            res.json(err.message);
+        }
+    }
+    
+    module.exports = scheduleOrderValidation;
